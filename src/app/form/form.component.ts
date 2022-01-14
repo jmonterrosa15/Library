01c2b92339e755f190fc5390f74a47855193ecbb
jmonterrosa15 <f.monterrosa3@gmail.com>
2022-01-14 09:31:09 -0500

    Form completed, ready to implement API to send data to database

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Book } from '../Book';
 
 @Component({
   selector: 'app-form',
@@ -8,18 +9,35 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  bookname = new FormControl('', [Validators.required]);
+  title = new FormControl('', [Validators.required]);
   author = new FormControl('', [Validators.required]);
+  numberOfPages = new FormControl('', [Validators.required]);
+  isRead = new FormControl();
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any) { 
-    
-   }
-  
+  form = new FormGroup({
+    title:this.title,
+    author:this.author,
+    numberOfPages:this.numberOfPages,
+    isRead:this.isRead
+  });
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any, private fb:FormBuilder) { 
+
+  }
+
   ngOnInit(): void {
   }
 
-  onAdd(name:string, author:string){
-    console.log(name, author);
+  
+
+  onAdd(form:any){
+    const book:Book = {
+      title: form.bookname,
+      author: form.author,
+      numberOfPages: form.numberOfPages,
+      isRead: form.isRead
+    }
+    console.log(form);
   }
 
 }
